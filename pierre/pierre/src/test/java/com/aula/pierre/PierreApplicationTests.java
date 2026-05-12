be7ff75ded734e397d210678975adfb199a1a10c
Marco Aurelio de Lima <158175292+spinhead007@users.noreply.github.com>
2026-05-12 15:54:27 -0300

    Inicio

---
+++ pierre/pierre/src/test/java/com/aula/pierre/PierreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.pierre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PierreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
